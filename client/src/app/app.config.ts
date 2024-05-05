31c9ff812417a312b503b593eff09d854f2af1aa
Drohul Oleh <oleh@drohul.com>
2024-05-05 17:12:07 +0300

    init

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,7 +2,22 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { GoogleLoginProvider } from '@abacritt/angularx-social-login';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(''),
+          },
+        ],
+      },
+    },
+  ],
 };
